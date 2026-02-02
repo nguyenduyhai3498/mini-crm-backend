f892cad59773991bec6629496c523fe5678d46a8
haind <haind@cfox.vn>
2026-02-02 09:43:59 +0700

    fix: api 3rd get tanent setting

--- src/open3rd/open3rd.controller.ts
+++ src/open3rd/open3rd.controller.ts
@@ -1,10 +1,12 @@
 import { Controller, Get, Query } from '@nestjs/common';
 import { Open3rdService } from './open3rd.service';
+import { Public } from 'src/auth/decorators/public.decorator';
 
 @Controller('open3rd')
 export class Open3rdController {
     constructor(private readonly open3rdService: Open3rdService) {}
 
+    @Public()
     @Get('settings')
     async getSettings(@Query('tenantId') tenantId: string) {
         return this.open3rdService.getSettings(tenantId);
